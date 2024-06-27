b82a16403d5f5c2750a6fb626ad62cf4e7d5176d
bikodor <124264640+bikodor@users.noreply.github.com>
2024-06-27 22:53:21 +0300

    Delete users directory

--- users/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-
-class User(AbstractUser):
-    photo = models.ImageField(upload_to="users/%Y/%m/%d/", blank=True, null=True, verbose_name="Фотография")
-    date_birth = models.DateTimeField(blank=True, null=True, verbose_name="Дата рождения")
-
-
